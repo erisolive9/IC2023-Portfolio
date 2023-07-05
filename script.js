1350e27f620036225619ef6b27425bfb77fa4815
Eris Blair <erisolive@gmail.com>
2023-07-04 20:26:11 -0400

    Redesign - 7/4
    Decided to change the design to a travel theme and remade the opening page and a profile. Need to figure out how to do transitions that look good between the pages next.

---
+++ script.js
@@ -0,0 +1,21 @@
+//Elements and divs on screen
+let intro = document.getElementById("intro");
+let profile = document.getElementById("profile");
+
+//Buttons
+let buttons = document.querySelector(".buttons");
+let continueButton1 = document.getElementById("openPassportButton");
+let continueButton2 = document.getElementById("boardButton");
+let backButton1 = document.getElementById("backButton1");
+
+continueButton1.onclick = function() {
+    intro.style.display = "none"
+    profile.style.display = "block"
+    buttons.style.display = "block"
+};
+
+backButton1.onclick = function() {
+    profile.style.display = "none"
+    buttons.style.display = "none"
+    intro.style.display = "flex"
+}
